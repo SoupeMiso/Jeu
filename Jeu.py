28000a7410e3c1e95482ac7a1b9967b9e50d78b8
Mozri <theo.loubet@orange.fr>
2016-07-01 18:00:31 +0200

    Update Jeu.py
    Ajouts mineurs

--- Jeu.py
+++ Jeu.py
@@ -8,6 +8,10 @@ fenetre = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Jeu qui n'a pas de nom")
 
 font = pygame.font.Font(None, 30)
+bulle1 = pygame.image.load("bulle1.png").convert()
+bulle2 = pygame.image.load("bulle2.png").convert()
+monsieur1 = pygame.image.load("monsieur1.png").convert()
+monsieur2 = pygame.image.load("monsieur2.png").convert()
 #bonjour a toi jeune poire
 mot1 = font.render(("Bonjour "),1,(255,255,255))
 mot2 = font.render(("à "),1,(255,255,255))
@@ -15,9 +19,13 @@ mot3 = font.render(("toi "),1,(255,255,255))
 mot4 = font.render(("jeune "),1,(255,255,255))
 mot5 = font.render(("poire "),1,(255,255,255))
 continuer = 1
-x = 50
-y = 50
+x = 1000
+y = 110
 mot = 1
+
+fenetre.blit(monsieur1, (x-100,y+150))
+fenetre.blit(bulle1, (x-40,y-60))
+pygame.display.flip()
 
 while continuer==1:
     pygame.time.Clock().tick(8)
